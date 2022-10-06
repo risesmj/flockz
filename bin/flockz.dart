8886b70085e60b5816bdde28961d525bdd731d59
MJ <matheusjcunha@gmail.com>
2022-10-06 17:04:23 -0300

    remove arguments fixed

--- bin/flockz.dart
+++ bin/flockz.dart
@@ -1,6 +1,5 @@
 import 'package:flock/app/program.dart';
 
 void main(List<String> arguments) {
-  arguments = ["-g", "map-exp", "Teste1/Teste2/FRONMAP.tlpp", "FRONMAP"];
   Program.execute(arguments);
 }
